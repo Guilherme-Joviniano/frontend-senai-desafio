1bfc961ae8756e85a5e9f7da3db32ad0e88ff5b4
Guilherme Joviniano <00drpixelss@gmail.com>
2022-09-18 16:12:08 -0700

    updating the detail page and tried to fix somes heights css issues

--- src/templates/detail/components/Grades/index.js
+++ src/templates/detail/components/Grades/index.js
@@ -1,3 +1,19 @@
-const render = (grades) => {
-    // render the grades
+const container = document.querySelector('.student-graph-grades')
+const MAX_AVERAGE = 100;
+
+const createGrade = ({ nome, media }) => {
+    const grade = document.createElement('progress')
+    
+    grade.max = MAX_AVERAGE
+    grade.value = media;
+    
+    return grade
 }
+const render = (grades = []) => {
+    // render the grades
+    grades.forEach(({ nome, media }) => {
+        container.append(createGrade({ nome, media }))
+    });
+}
+
+export default render;
--- src/templates/detail/components/Profile/index.js
+++ src/templates/detail/components/Profile/index.js
@@ -1,4 +1,11 @@
 
-const render = (picture, name) => {
+const render = (props) => {
     // render the profile
+    const { foto, nome } = props;
+    
+    document.querySelector('.picture').src = foto; 
+    document.querySelector('.name').textContent = nome;
 }
+
+
+export default render
